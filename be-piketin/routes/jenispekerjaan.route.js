8a548c7a5a69305f0de393e44fd6956e4dc6f0d1
sashenkaze <sashenkaosazee@gmail.com>
2026-05-12 19:08:41 +0700

    feat: add jenispekerjaan route

--- be-piketin/routes/jenispekerjaan.route.js
+++ be-piketin/routes/jenispekerjaan.route.js
@@ -1,4 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const { checkToken, checkRole } = require('../middlewares/auth')
+const jenispekerjaanController = require('../controllers/jenispekerjaan.controller')
+const upload = require('../middlewares/upload')
+
+router.get('/', checkToken, checkRole('admin'), jenispekerjaanController.getAllJenisPekerjaan)
+router.get('/:id', checkToken, checkRole('admin'), jenispekerjaanController.getJenisPekerjaanById)
+router.post('/', checkToken, checkRole('admin'), upload.none(), jenispekerjaanController.createJenisPekerjaan)
+router.put('/:id', checkToken, checkRole('admin'), upload.none(), jenispekerjaanController.updateJenisPekerjaan)
+router.delete('/:id', checkToken, checkRole('admin'), jenispekerjaanController.deleteJenisPekerjaan)
+
 module.exports = router
