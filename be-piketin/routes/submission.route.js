b25a6d4785a2886d34168030e0fff4d54e7d6e9a
sashenkaze <sashenkaosazee@gmail.com>
2026-05-13 10:45:43 +0700

    feat: add submission route

--- be-piketin/routes/submission.route.js
+++ be-piketin/routes/submission.route.js
@@ -1,4 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const { checkToken, checkRole } = require('../middlewares/auth')
+const submissionController = require('../controllers/submission.controller')
+const upload = require('../middlewares/upload')
+
+//! upload.fields() : upload lebih dari 1 file sekaligus dgn nama field beda
+//! berbeda dr upload.single() untuk 1 file aja
+router.post('/', checkToken, checkRole('murid'), upload.fields([
+    { name: 'foto_sebelum', maxCount: 1 },
+    { name: 'foto_sesudah', maxCount: 1 },
+]), submissionController.createSubmission)
+router.get('/', checkToken, checkRole('admin'), submissionController.getAllSubmissions)
+router.get('/my', checkToken, checkRole('murid'), submissionController.getMySubmission)
+router.put('/:id/status', checkToken, checkRole('admin'), upload.none(), submissionController.updateStatus)
+
 module.exports = router
